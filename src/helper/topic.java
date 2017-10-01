40c8d67582badad930ed398e77932c7dec137f1a
Shubham <shub.shubhamthakur@gmail.com>
2017-10-01 23:41:59 +0530

    topic class created

---
+++ src/helper/topic.java
@@ -0,0 +1,31 @@
+package helper;
+
+import java.util.*;
+
+public class topic{
+	int id;
+	ArrayList<topic> ar;
+	String data;
+	
+	public topic(){
+		ar = new ArrayList<topic>();
+	}
+	public int getId(){
+		return id;
+	}
+	public void setId(int i){
+		id = i;
+	}
+	public String getData(){
+		return data;
+	}
+	public void setData(String str){
+		data = str;
+	}
+	public ArrayList getSub(){
+		return ar;
+	}
+	public void setSub(topic t){
+		ar.add(t);
+	}
+}
