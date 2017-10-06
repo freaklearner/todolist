a039b91ec2daec446cb67f800205b2f4ea406e8c
Shubham <shub.shubhamthakur@gmail.com>
2017-10-06 21:29:37 +0530

    servlet file modified to dispatch to jsp view

--- src/com/serv.java
+++ src/com/serv.java
@@ -10,15 +10,15 @@ import java.sql.*;
 
 public class serv extends HttpServlet{
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("start");
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		//out.println("start");
 		eval ob = new eval();
 		String d = (String)getServletContext().getInitParameter("driver");
 		String p = (String)getServletContext().getInitParameter("path");
 		try{
 			ArrayList<topic> list = ob.inital(d,p,1001);
-			for(int i=0;i<list.size();i++){
+			/*for(int i=0;i<list.size();i++){
 				topic t = list.get(i);
 				out.print(t.getData()+"<br/>");
 				ArrayList<topic> sub1 = t.getSub();
@@ -30,8 +30,10 @@ public class serv extends HttpServlet{
 					}
 				}
 			}
+			*/
+			request.setAttribute("topics",list);
 		}catch(Exception ex){
-			out.println(ex);
+			//out.println(ex);
 			//out.print(ex.getMessage());
 			/*StringWriter sw = new StringWriter();
 			PrintWriter pw = new PrintWriter(sw);
@@ -39,6 +41,10 @@ public class serv extends HttpServlet{
 			String sStackTrace = sw.toString(); // stack trace as a string
 			System.out.println(sStackTrace);
 			*/
+			request.setAttribute("error",ex);
 		}
+		
+		RequestDispatcher view = request.getRequestDispatcher("main.jsp");
+		view.forward(request,response);
 	}
 }
